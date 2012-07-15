c83c71c1b840a70a27ff56bef4b775f60e6774b4
Jürgen Bickert <juergenbickert@gmail.com>
2012-07-15 22:35:16 +0200

    update

--- quacks-web.lisp
+++ quacks-web.lisp
@@ -11,9 +11,9 @@
                           :worker-threads 1))
 
 (defvar *logger* (open (relative-file "log")
-                             :direction :output
-                             :if-exists :append
-                             :if-does-not-exist :create))
+                       :direction :output
+                       :if-exists :append
+                       :if-does-not-exist :create))
 
 (defvar *db* (make-instance 'memory-db))
 
@@ -34,38 +34,38 @@
 
 (defun handle-request (stream)
   (let ((req (http-protocol-reader stream)))
-   (handler-case
-       (let ((res (list (cons :version (cdr (assoc :version req)))
-                        (cons :status 200)
-                        (cons "Content-Type" "text/html")))
-             (parameters (cdr (assoc :parameters req))))
-         (write-log req)
-         (http-protocol-writer
-          res
-          (switch ((cdr (assoc :url req)) :test 'equal)
-            ("/authors" (let ((*authors* (get-authors *db*)))
-                          (render-template *authors-template*)))
-            ("/author/:id" (let ((*author* (get-author (parameter :id parameters) *db*)))
-                              (render-template *author-template*)))
-            ("/users" (let ((*users* (get-users *db*)))
-                        (render-template *users-template*)))
-            ("/user/:id" (let ((*user* (get-user (parameter :id parameters) *db*)))
-                           (render-template *user-template*)))
-            ("/stylesheet/image.css" (progn
-                                       (setf (cdr (assoc "Content-Type" res :test 'equal)) "text/css")
-                                       (inline-file-template (relative-file "public/stylesheet/image.css"))))
-            (t (progn
-                 (setf (cdr (assoc :status res)) 404)
-                 "404 Not Found")))
-          stream))
-     (t (e)
-       (let* ((res (list (cons :version (cdr (assoc :version req)))
+    (handler-case
+        (let ((res (list (cons :version (cdr (assoc :version req)))
                          (cons :status 200)
-                         (cons "Content-Type" "text/plain"))))
-         (http-protocol-writer
-          res
-          (format nil "~w~%~%~a" req e)
-          stream))))))
+                         (cons "Content-Type" "text/html")))
+              (parameters (cdr (assoc :parameters req))))
+          (write-log req)
+          (http-protocol-writer
+           res
+           (switch ((cdr (assoc :url req)) :test 'equal)
+             ("/authors" (let ((*authors* (get-authors *db*)))
+                           (render-template *authors-template*)))
+             ("/author/:id" (let ((*author* (get-author (parameter :id parameters) *db*)))
+                              (render-template *author-template*)))
+             ("/users" (let ((*users* (get-users *db*)))
+                         (render-template *users-template*)))
+             ("/user/:id" (let ((*user* (get-user (parameter :id parameters) *db*)))
+                            (render-template *user-template*)))
+             ("/stylesheet/image.css" (progn
+                                        (setf (cdr (assoc "Content-Type" res :test 'equal)) "text/css")
+                                        (inline-file-template (relative-file "public/stylesheet/image.css"))))
+             (t (progn
+                  (setf (cdr (assoc :status res)) 404)
+                  "404 Not Found")))
+           stream))
+      (t (e)
+        (let* ((res (list (cons :version (cdr (assoc :version req)))
+                          (cons :status 200)
+                          (cons "Content-Type" "text/plain"))))
+          (http-protocol-writer
+           res
+           (format nil "~w~%~%~a" req e)
+           stream))))))
 
 (defun write-log (args)
   (format *logger* "~w~%" args))
