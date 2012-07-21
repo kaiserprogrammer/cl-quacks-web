26b4970246e3782d885593e455bcca56b937d0df
Jürgen Bickert <juergenbickert@gmail.com>
2012-07-21 21:12:37 +0200

    use new stampede api for http server

--- quacks-web.lisp
+++ quacks-web.lisp
@@ -10,10 +10,7 @@
 (in-package :quacks-web)
 
 (defvar *server*
-  (stampede:create-server 8000
-                          (lambda (stream)
-                            (handle-request stream))
-                          :worker-threads 20))
+  (stampede:create-http-server 8000 :worker-threads 20))
 
 ;; (defvar *logger* (open (relative-file "log")
 ;;                        :direction :output
@@ -78,80 +75,48 @@
 ;; (defrenderer (relative-file "users"))
 ;; (defrenderer (relative-file "images"))
 
-(defparameter *routes* (list (list "GET")
-                             (list "POST")
-                             (list "PUT")))
 
-(defun defroute (method reg fun)
-  (let ((regex (regex-replace-all ":[^/]+" reg "([^/$]+)"))
-        (params (extract-params-from-regex reg)))
-    (let ((item (cons regex (cons params fun))))
-      (push item (cdr (assoc method *routes* :test #'string=))))))
-
-(defun extract-params-from-regex (reg)
-  (let (params)
-    (do-register-groups (param) ("/:([^/$]+)" reg)
-      (push (make-keyword (string-upcase param)) params))
-    (nreverse params)))
-
-(defun call-route (req res)
-  (declare (optimize (debug 3)))
-  (let* ((url (cdr (assoc :url req)))
-         (method (cdr (assoc :method req))))
-    (loop for route in (cdr (assoc method *routes* :test #'string=))
-       for (match . groups) = (multiple-value-bind (match groups)
-                                  (scan-to-strings (car route) url)
-                                (cons match groups))
-       when match
-       return (progn
-                (when (not (emptyp groups))
-                  (loop for value across (the simple-vector groups)
-                     for key in (cadr route)
-                     do (push (cons key value) (cdr (assoc :params req)))))
-                (funcall (the function (cddr route)) req res)))))
-
-
-(defroute "GET" "^/authors$" (lambda (req res)
+(defroute *server* "GET" "^/authors$" (lambda (req res)
                               (declare (ignorable res req))
                               (let ((*authors* (get-authors *db*))
                                     (*title* "Authors"))
                                 (render-authors-index))))
-(defroute "GET" "^/authors/:id$" (lambda (req res)
+(defroute *server* "GET" "^/authors/:id$" (lambda (req res)
                                   (declare (ignorable res req))
                                   (let* ((id (parse-integer (parameter :id (cdr (assoc :params req)))))
                                          (*author* (get-author id *db*)))
                                     (render-authors-show))))
-(defroute "GET" "^/authors/new" (lambda (req res)
+(defroute *server* "GET" "^/authors/new" (lambda (req res)
                                   (declare (ignorable req res))
                                   (render-authors-new)))
-(defroute "POST" "^/authors" (lambda (req res)
+(defroute *server* "POST" "^/authors" (lambda (req res)
                                (declare (ignorable req res)
                                         (optimize (debug 3)))
                                (add-author (cdr  (assoc "name" (cdr (assoc :params req)) :test #'string=)) *db*)
                                (redirect-to "/authors")))
 
-(defroute "GET" "^/users/:id$" (lambda (req res)
+(defroute *server* "GET" "^/users/:id$" (lambda (req res)
                                 (declare (ignorable res req))
                                 (let ((*user* (get-user (parse-integer (parameter :id (cdr (assoc :params req)))) *db*)))
                                   (render-users-show))))
 
 
-(defroute "GET" "^/public/quacks.css$" (lambda (req res)
+(defroute *server* "GET" "^/public/quacks.css$" (lambda (req res)
                                          (declare (ignorable res req))
                                          (setf (cdr (assoc "Content-Type" res :test #'string=)) "text/css")
                                          (alexandria:read-file-into-string (relative-file "public/quacks.css"))))
 
-(defroute "GET" "^/public/quacks.js$" (lambda (req res)
+(defroute *server* "GET" "^/public/quacks.js$" (lambda (req res)
                                        (declare (ignorable res req))
                                        (setf (cdr (assoc "Content-Type" res :test #'string=)) "text/javascript")
                                        (alexandria:read-file-into-string (relative-file "public/quacks.js"))))
 
-(defroute "GET" "^/images/:id$" (lambda (req res)
+(defroute *server* "GET" "^/images/:id$" (lambda (req res)
                                   (declare (ignorable res req))
                                   (let ((*author-id* (parameter :id (cdr (assoc :params req)))))
                                     (render-images-edit))))
 
-(defroute "PUT" "^/images/:id$" (lambda (req res)
+(defroute *server* "PUT" "^/images/:id$" (lambda (req res)
                                   (declare (ignorable res req))
                                   (let ((author-id (parse-integer (parameter :id (cdr (assoc :params req)))))
                                         (url (cdr (assoc "url" (cdr (assoc :params req)) :test #'string=))))
@@ -159,7 +124,7 @@
                                       (add-image author-id url *db*))
                                     (redirect-to (format nil "/authors/~a" author-id)))))
 
-(defroute "GET" "^/users$" (lambda (req res)
+(defroute *server* "GET" "^/users$" (lambda (req res)
                             (declare (ignorable res req))
                             (let ((*users* (get-users *db*)))
                               (render-users-index))))
@@ -177,26 +142,6 @@
 (defvar *author-id* nil)
 (defvar *title* nil)
 
-(defun handle-request (stream)
-  (let ((req (http-protocol-reader stream)))
-    (handler-case
-        (let ((res (list (cons :version (cdr (assoc :version req)))
-                         (cons :status 200)
-                         (cons "Content-Type" "text/html"))))
-          ;; (write-log req)
-          (http-protocol-writer res
-                                (call-route req res)
-                                stream))
-      (t (e)
-        (let ((res (list (cons :version (cdr (assoc :version req)))
-                         (cons :status 200)
-                         (cons "Content-Type" "text/plain"))))
-          (http-protocol-writer res
-                                (format nil "~w~%~%~a" req e)
-                                stream))))))
-
-;; (defun write-log (args)
-;;   (format *logger* "~w~%" args))
 
 (defun parameter (name parameters)
   (cdr (assoc name parameters)))
